f0a7d2d9a0f0764ae5751ad480ebb1e0ccbc0e57
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-10 22:05:28 -0300

    criando a pagina do plugin => tem problemas devido desatualizacao

--- src/plugins/repositories/admin/src/pages/HomePage/index.js
+++ src/plugins/repositories/admin/src/pages/HomePage/index.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import styled from 'styled-components';
-// import { HeaderNav, HeaderModalTitle } from 'strapi-helper-plugin';
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+import axios from 'axios';
 
 const Wrapper = styled.div`
   padding: 18px 30px;
 
+  p {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
 `;
 
 const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.github.com/users/React-avancado/repos")
+      .then((res) => setRows(res.data))
+      .catch(e => strapi.notification.error(`Ops... github API limit exceeded, ${e}`))
+  }, []);
+
+  const headers = [
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Description",
+      value: "description",
+    },
+    {
+      name: "Url",
+      value: "html_url",
+    },
+  ];
+
   return (
     <Wrapper>
-      {/* <HeaderModalTitle
-
+      <Header
         title={{ label: "React Avançado Repositories" }}
         content="A list of our repositories in React Avançado course."
-      /> */}
+      />
+      <Table headers={headers} rows={rows} />
     </Wrapper>
   );
 };
 
-export default HomePage;
+export default memo(HomePage);
