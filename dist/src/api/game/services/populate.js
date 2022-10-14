dcd24eeb6fd792f1da7d49a5bc2fefe48e0cdd0d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-13 21:36:56 -0300

    aula127 - fazendo scrapper com jsdom

--- dist/src/api/game/services/populate.js
+++ dist/src/api/game/services/populate.js
@@ -4,11 +4,23 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios = require("axios");
+async function getGameInfo(slug) {
+    const jsdom = require("jsdom");
+    const { JSDOM } = jsdom;
+    const body = await axios.get(`https://www.gog.com/en/game/${slug}`);
+    const dom = new JSDOM(body.data);
+    const description = dom.window.document.querySelector('.description');
+    return {
+        rating: 'BR0',
+        short_description: description.textContent.slice(0, 160),
+        description: description.innerHTML
+    };
+}
 exports.default = () => ({
     async populate(params) {
         // const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
         const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
         const { data: { products } } = await axios.get(gogApiUrl);
-        console.log(products[0]);
+        console.log(await getGameInfo(products[0].slug));
     }
 });
--- src/api/game/services/populate.ts
+++ src/api/game/services/populate.ts
@@ -5,6 +5,21 @@
 const axios = require("axios");
 
 
+async function getGameInfo(slug){
+  const jsdom = require("jsdom");
+  const {JSDOM} = jsdom;
+  const body = await axios.get(`https://www.gog.com/en/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector('.description');
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0,160),
+    description: description.innerHTML
+  }
+}
+
 export default () => ({
   async populate(params) {
     // const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
@@ -12,6 +27,6 @@ export default () => ({
 
     const {data:{products}} = await axios.get(gogApiUrl)
 
-    console.log(products[0])
+    console.log(await getGameInfo(products[0].slug))
   }
 });
