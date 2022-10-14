ab6f7372d6451c5924b2431039691e530764981d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-13 21:21:25 -0300

    aula126 - fazendo fetch nos dados da API da GoG

--- src/api/game/services/populate.ts
+++ src/api/game/services/populate.ts
@@ -2,9 +2,16 @@
  * populate service
  */
 
+const axios = require("axios");
+
+
 export default () => ({
   async populate(params) {
-    const cat = await strapi.service('api::category.category').find({name: "Action"})
-    console.log(cat)
+    // const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+
+    const {data:{products}} = await axios.get(gogApiUrl)
+
+    console.log(products[0])
   }
 });
