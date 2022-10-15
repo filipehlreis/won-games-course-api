f85bc33a492d7ac083fbe835bcc385cea457a087
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-15 16:28:06 -0300

    aula128 - criando dados com service.create

--- dist/src/api/game/services/populate.js
+++ dist/src/api/game/services/populate.js
@@ -17,24 +17,30 @@ async function getGameInfo(slug) {
         description: description.innerHTML
     };
 }
+async function getByName(name, entityName) {
+    // const item = await strapi.entityService.findMany()
+    const item = await strapi.services[entityName].find({ name });
+    return item.length ? item[0] : null;
+}
 exports.default = () => ({
     async populate(params) {
         // const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
         const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
         const { data: { products } } = await axios.get(gogApiUrl);
         console.log(products[0]);
-        await strapi.entityService.create('api::publisher.publisher', {
-            data: {
-                name: products[0].publisher,
-                slug: slugify(products[0].publisher).toLowerCase(),
-            }
-        });
-        await strapi.entityService.create('api::developer.developer', {
-            data: {
-                name: products[0].developer,
-                slug: slugify(products[0].developer).toLowerCase(),
-            }
-        });
+        console.log(await getByName(products[0].publisher, "publisher"));
+        // await strapi.entityService.create('api::publisher.publisher', {
+        //   data:{
+        //     name: products[0].publisher,
+        //     slug: slugify(products[0].publisher).toLowerCase(),
+        //   }
+        // });
+        // await strapi.entityService.create('api::developer.developer', {
+        //   data:{
+        //     name: products[0].developer,
+        //     slug: slugify(products[0].developer).toLowerCase(),
+        //   }
+        // });
         // console.log(await getGameInfo(products[0].slug))
     }
 });
--- src/api/game/services/populate.ts
+++ src/api/game/services/populate.ts
@@ -20,6 +20,13 @@ async function getGameInfo(slug){
   };
 }
 
+async function getByName(name, entityName){
+  // const item = await strapi.entityService.findMany()
+  const item = await strapi.services[entityName].find({name})
+  return item.length ? item[0] : null;
+}
+
+
 export default () => ({
   async populate(params) {
     // const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
@@ -29,19 +36,21 @@ export default () => ({
 
     console.log(products[0]);
 
-    await strapi.entityService.create('api::publisher.publisher', {
-      data:{
-        name: products[0].publisher,
-        slug: slugify(products[0].publisher).toLowerCase(),
-      }
-    });
+    console.log(await getByName(products[0].publisher,"publisher"))
 
-    await strapi.entityService.create('api::developer.developer', {
-      data:{
-        name: products[0].developer,
-        slug: slugify(products[0].developer).toLowerCase(),
-      }
-    });
+    // await strapi.entityService.create('api::publisher.publisher', {
+    //   data:{
+    //     name: products[0].publisher,
+    //     slug: slugify(products[0].publisher).toLowerCase(),
+    //   }
+    // });
+
+    // await strapi.entityService.create('api::developer.developer', {
+    //   data:{
+    //     name: products[0].developer,
+    //     slug: slugify(products[0].developer).toLowerCase(),
+    //   }
+    // });
 
     // console.log(await getGameInfo(products[0].slug))
   }
