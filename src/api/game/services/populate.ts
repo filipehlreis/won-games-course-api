c11be3e69388077e670fbd15aef23daac1ab7882
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-17 19:48:51 -0300

    removendo comentario em populate.ts

--- src/api/game/services/populate.ts
+++ src/api/game/services/populate.ts
@@ -45,7 +45,6 @@ async function create(name, entityName){
 
 export default () => ({
   async populate(params) {
-    // const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
 
     const {data:{products}} = await axios.get(gogApiUrl);
