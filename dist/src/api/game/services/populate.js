dc6b67ff8046540e2e3715bc9efd09ebb526e302
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-10-19 23:54:26 -0300

    passando de draft&publsih para publish diretamente

--- dist/src/api/game/services/populate.js
+++ dist/src/api/game/services/populate.js
@@ -67,7 +67,7 @@ async function setImage({ image, game, field = "cover" }) {
     const FormData = require("form-data");
     const formData = new FormData();
     formData.append("refId", game.id);
-    formData.append("ref", "game");
+    formData.append("ref", "api::game.game");
     formData.append("field", field);
     formData.append("files", buffer, { filename: `${game.slug}.jpg` });
     console.info(`\n\nUploading ${field} image: ${game.slug}.jpg`);
