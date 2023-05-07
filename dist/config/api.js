6a792818ed55c80682a54c5b8a335d8c45f40a19
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-05-07 12:44:57 -0300

    feat(aula451): config Nodemailer on Strapi and intercept with MailDev

--- dist/config/api.js
+++ dist/config/api.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     rest: {
         defaultLimit: 25,
-        maxLimit: 100,
+        maxLimit: 10000,
         withCount: true,
     },
 };
