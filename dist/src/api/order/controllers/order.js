1cfb8d2baa62792fef67e00e588f91a521beae62
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-06-22 23:33:37 -0300

    feat(aula494): search data from cart in backend

--- dist/src/api/order/controllers/order.js
+++ dist/src/api/order/controllers/order.js
@@ -7,7 +7,26 @@ const strapi_1 = require("@strapi/strapi");
 exports.default = strapi_1.factories.createCoreController('api::order.order', ({ strapi }) => ({
     // Method 1: Creating an entirely custom action
     async createPaymentIntent(ctx) {
-        const { cart } = ctx.request.body;
-        return cart;
+        try {
+            const { cart } = ctx.request.body;
+            let games = [];
+            // await strapi.entityService.findMany(`api::${entityName}.${entityName}`, {
+            //   fields: ['name'],
+            //   filters: { name: name },
+            //   sort: 'name',
+            // })
+            await Promise.all(cart === null || cart === void 0 ? void 0 : cart.map(async (game) => {
+                const validatedGame = await strapi.entityService.findOne('api::game.game', game.id, {});
+                if (validatedGame) {
+                    games.push(validatedGame);
+                }
+                console.log(validatedGame);
+            }));
+            return games;
+        }
+        catch (error) {
+            console.log(error);
+            return "Deu ruim";
+        }
     },
 }));
--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -4,11 +4,49 @@
 
 import { factories } from '@strapi/strapi'
 
+export type gameProps = {
+  id: string
+}
+
+export type cartProps = {
+  cart?: [gameProps] | null
+}
+
+
+
 export default factories.createCoreController('api::order.order', ({ strapi }) => ({
   // Method 1: Creating an entirely custom action
   async createPaymentIntent(ctx) {
-    const { cart } = ctx.request.body;
 
-    return cart;
+    try {
+
+      const { cart }: cartProps = ctx.request.body;
+
+      let games = []
+
+
+      // await strapi.entityService.findMany(`api::${entityName}.${entityName}`, {
+      //   fields: ['name'],
+      //   filters: { name: name },
+      //   sort: 'name',
+      // })
+
+      await Promise.all(
+        cart?.map(async (game) => {
+          const validatedGame = await strapi.entityService.findOne('api::game.game', game.id
+            , {})
+          if (validatedGame) {
+            games.push(validatedGame)
+          }
+
+          console.log(validatedGame)
+        })
+      )
+      return games;
+
+    } catch (error) {
+      console.log(error)
+      return "Deu ruim"
+    }
   },
 }));
